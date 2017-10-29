a602d5d54f6927f8aa88b2135075ca6ec7072d43
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-10-29 04:33:19 -0700

    added liri.js

---
+++ liri.js
@@ -0,0 +1,129 @@
+#!/usr/bin/env node
+var action = process.argv[2];
+var value = process.argv[3];
+var Twitter = require('twitter');
+var keys = require('./keys');
+var Spotify = require('node-spotify-api')
+var client = new Twitter(keys.twitterKeys);
+var params = {
+    screen_name: 'ralph_complido',
+    count: 20
+    }
+var spotify = new Spotify({
+  id: '47034d83e4be45eb9890aac813bc423f',
+  secret: 'ab7df4a108c34700b1f6ae57db509326',
+
+});
+var request = require('request');
+var fs = require('fs');
+
+switch (action) {
+    case 'mytweets':
+        myTweets();
+        break;
+    case 'spotify':
+        SpotifySearch(value);
+        break;
+    case 'omdb':
+        omdbThis(value);
+        break;
+    case 'random':
+        random();
+        break;
+}
+
+// my-tweets function
+function myTweets() {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error && response.statusCode == 200) {
+            fs.appendFile('terminal.log', ('=============== LOG ENTRY BEGIN ===============\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS:\r\n$: ' + process.argv + '\r\n \r\nDATA OUTPUT:\r\n'), function(err) {
+                if (err) throw err;
+            });
+            console.log(' ');
+            console.log('Last 20 Tweets:')
+            for (i = 0; i < tweets.length; i++) {
+                var number = i + 1;
+                console.log(' ');
+                console.log([i + 1] + '. ' + tweets[i].text);
+                console.log('Created on: ' + tweets[i].created_at);
+                console.log(' ');
+                fs.appendFile('terminal.log', (number + '. Tweet: ' + tweets[i].text + '\r\nCreated at: ' + tweets[i].created_at + ' \r\n'), function(err) {
+                    if (err) throw err;
+                });
+            }
+            fs.appendFile('terminal.log', ('=============== LOG ENTRY END ===============\r\n \r\n'), function(err) {
+                if (err) throw err;
+            });
+        }
+    });
+} // end myTweets function
+
+// spotifyThis function
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
+function SpotifySearch(value) {
+    spotify.search({ type: 'track', query: value }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+jsonBody = JSON.parse(data);
+            console.log(' ');
+            console.log('Artist: ' + jsonBody.tracks.items[0].artists[0].name);
+            console.log('Song: ' + jsonBody.tracks.items[0].name);
+            console.log('Preview Link: ' + jsonBody.tracks.items[0].preview_url);
+            console.log('Album: ' + jsonBody.tracks.items[0].album.name);
+            console.log(' ');
+            fs.appendFile('terminal.log', ('=============== LOG ENTRY BEGIN ===============\r\n' + Date() +'\r\n \r\nTERMINAL COMMANDS:\r\n$: ' + process.argv + '\r\n \r\nDATA OUTPUT:\r\n' + 'Artist: ' + jsonBody.tracks.items[0].artists[0].name + '\r\nSong: ' + jsonBody.tracks.items[0].name + '\r\nPreview Link: ' + jsonBody.tracks.items[0].preview_url + '\r\nAlbum: ' + jsonBody.tracks.items[0].album.name + '\r\n=============== LOG ENTRY END ===============\r\n \r\n'), function(err) {
+                if (err) throw err;
+            });
+});
+} 
+// omdbThis function
+function omdbThis(value) {
+    if (value == null) {
+        value = 'wargames';
+    }
+    request('http://www.omdbapi.com/?t=' + value + '&tomatoes=true&r=json', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            jsonBody = JSON.parse(body);
+            console.log(' ');
+            console.log('Title: ' + jsonBody.Title);
+            console.log('Year: ' + jsonBody.Year);
+            console.log('IMDb Rating: ' + jsonBody.imdbRating);
+            console.log('Country: ' + jsonBody.Country);
+            console.log('Language: ' + jsonBody.Language);
+            console.log('Plot: ' + jsonBody.Plot);
+            console.log('Actors: ' + jsonBody.Actors);
+            console.log('Rotten Tomatoes Rating: ' + jsonBody.tomatoRating);
+            console.log('Rotten Tomatoes URL: ' + jsonBody.tomatoURL);
+            console.log(' ');
+            fs.appendFile('log.txt', ('=============== LOG ENTRY BEGIN ===============\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS: ' + process.argv + '\r\nDATA OUTPUT:\r\n' + 'Title: ' + jsonBody.Title + '\r\nYear: ' + jsonBody.Year + '\r\nIMDb Rating: ' + jsonBody.imdbRating + '\r\nCountry: ' + jsonBody.Country + '\r\nLanguage: ' + jsonBody.Language + '\r\nPlot: ' + jsonBody.Plot + '\r\nActors: ' + jsonBody.Actors + '\r\nRotten Tomatoes Rating: ' + jsonBody.tomatoRating + '\r\nRotten Tomatoes URL: ' + jsonBody.tomatoURL + '\r\n =============== LOG ENTRY END ===============\r\n \r\n'), function(err) {
+                if (err) throw err;
+            });
+        }
+    });
+} //end omdbThis function
+
+// random function
+function random() {
+    fs.readFile('random.txt', 'utf8', function(error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            var dataArr = data.split(',');
+            if (dataArr[0] === 'spotify') {
+                spotifyThis(dataArr[1]);
+            }
+            if (dataArr[0] === 'omdb') {
+                omdbThis(dataArr[1]);
+            }
+        }
+    });
+} // end doWhatItSays function
